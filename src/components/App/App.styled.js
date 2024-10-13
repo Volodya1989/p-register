42263c2761bcb9b54127bd9a11484f0f056938a1
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2024-10-13 07:37:48 -0400

    server added

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -20,10 +20,4 @@ export const Phonebook = styled.div`
   background: linear-gradient(to bottom right, #f5f7fa, #b8c6db);
 `;
 
-export const Contacts = styled.div`
-  box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
-  border-radius: 10px;
-  padding: 20px;
-  background: linear-gradient(to bottom right, #fffcff, #d5fefd);
-  margin-top: 50px;
-`;
+
--- src/redux/phonebook/operations.js
+++ src/redux/phonebook/operations.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-// axios.defaults.baseURL = "https://p-register-server.onrender.com";
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://p-register-server.onrender.com";
+// axios.defaults.baseURL = "http://localhost:3001";
 
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
