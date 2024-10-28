556c84d3a0bab1c0f97ece26c8102e9fdea96089
Volodya1989 <Vpetrytsia@gmail.com>
2024-10-28 01:19:29 +0000

    Deploying to gh-pages from @ Volodya1989/p-register@330b5fcc8b2f7e073b1328e23aed122eece01b28 🚀

---
+++ static/js/354.99dcf49f.chunk.js
@@ -0,0 +1,163 @@
+"use strict";(self.webpackChunkp_register=self.webpackChunkp_register||[]).push([[354],{4013:(e,t,a)=>{a.d(t,{mc:()=>n});var r=a(1529);const n=r.Ay.div`
+  display: block;
+  text-align: center;
+  width: 700px;
+  margin-right: auto;
+  margin-left: auto;
+  padding: 30px;
+`;r.Ay.input`
+  width: 300px;
+`,r.Ay.div`
+  box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+  border-radius: 10px;
+  padding: 20px;
+  background: linear-gradient(to bottom right, #f5f7fa, #b8c6db);
+`},8122:(e,t,a)=>{a.d(t,{A:()=>o});var r=a(5508);const n=a(1529).Ay.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 15%;
+`;var i=a(579);const o=()=>(0,i.jsx)(n,{children:(0,i.jsx)(r.IN,{strokeColor:"#f4c550",margin:"auto",textAlign:"center",strokeWidth:"5",animationDuration:"0.75",width:"96",visible:!0})})},5354:(e,t,a)=>{a.r(t),a.d(t,{default:()=>z});var r=a(1591),n=a(7614),i=a(8560),o=a(5043),s=a(8298),l=a(4013);const c=e=>e.baptisms;var u=a(7209),h=a(4858),m=a(7933),d=a(6971),x=a(3217),p=a(5030),f=a(9302),g=a(8390),v=a(1529);v.Ay.img`
+  positon: relative;
+`;const j=v.Ay.div`
+  position: relative;
+  display: block;
+  margin: auto;
+  margin-top: 100px;
+  width: 100%;
+  max-width: 1100px;
+  background-color: #f7f7f7;
+  padding: 40px 40px;
+  border-radius: 30px;
+  box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+
+  @media screen and (min-width: 768px) {
+    font-size: 30px;
+    line-height: 1.07;
+    width: 600px;
+  }
+  @media screen and (min-width: 1100px) {
+    font-size: 30px;
+    line-height: 1.06;
+  }
+`,b=(v.Ay.p`
+  font-size: var(--subheadingFonSize);
+  line-height: 1.18;
+  margin-top: 10px;
+  margin-bottom: 25px;
+`,v.Ay.h3`
+  font-size: 40px;
+  line-height: calc(48 / 40);
+  font-weight: 500;
+  margin-bottom: 25px;
+  text-align: left;
+  letter-spacing: -0.02em;
+`),y=v.Ay.h6`
+  font-size: 25px;
+  line-height: calc(48 / 40);
+  font-weight: 500;
+  margin-bottom: 25px;
+  margin-top: 15px;
+  text-align: left;
+
+  letter-spacing: -0.02em;
+`,C=v.Ay.h6`
+  font-size: 20px;
+  line-height: calc(38 / 30);
+  font-weight: 500;
+  margin-bottom: 15px;
+  margin-top: 5px;
+  text-align: left;
+
+  letter-spacing: -0.02em;
+`,N=(v.Ay.p`
+  font-size: 16px;
+  font-weight: 400;
+  line-height: calc(22 / 16);
+  text-align: left;
+`,v.Ay.label`
+  font-size: 18px;
+  font-weight: 300;
+  width: 200px;
+  color: #9ca5b5;
+  position: absolute;
+  left: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  transition: all 0.2s ease;
+  pointer-events: none;
+  padding-left: 15px;
+`),k=v.Ay.section`
+  flex-direction: "row";
+  alignitems: "center";
+  position: relative;
+  margin-top: 30px;
+`,A=(v.Ay.img`
+  border-radius: 50%;
+  width: 44px;
+  height: 44px;
+`,v.Ay.input`
+  display: flex;
+  border: 0;
+  font-size: 22px;
+  background-color: transparent;
+  padding-top: 10px;
+  border-bottom: 1px solid #f4c550;
+
+  outline: none;
+  width: 100%;
+  color: black;
+  overflow: hidden;
+
+  ${e=>{let{disabled:t}=e;return t?"\n    pointer-events: none;\n    \n  ":""}}
+
+  &:focus {
+    border-color: #6118de;
+  }
+
+  &:focus ~ ${N} {
+    transform: translateY(-40px);
+    // scale: 0.9;
+    color: #9ca5b5;
+  }
+
+  ${e=>{let{value:t=""}=e;return t&&`\n    & ~ ${N} {\n      transform: translateY(-40px);\n\n    }\n  `}}
+`),S=v.Ay.p`
+  font-size: 15px;
+  line-height: calc(18 / 12);
+  color: red;
+`,F=v.Ay.input`
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  height: Hug (60px) px;
+  padding: 16px 48px 16px 48px;
+  gap: 0px;
+  display: flex;
+  justify-content: center;
+  margin-top: 25px;
+  border-radius: 12px;
+  opacity: 0px;
+  font-weight: 700;
+  background: #f4c550;
+  border: none;
+  font-size: 18px;
+  line-height: calc(28 / 18);
+  cursor: pointer;
+  &:hover {
+    transition: opacity 0.4s;
+    opacity: 0.8;
+    transition-timing-function: ease;
+  }
+`,w=(v.Ay.div`
+  position: absolute;
+  top: 0;
+  right: 20px;
+  cursor: pointer;
+`,v.Ay.div`
+  position: absolute;
+  z-index: 10;
+  top: 1em;
+  left: 8em;
+`);var T=a(8122),q=a(579);const B=()=>{const e=(0,s.wA)(),{register:t,handleSubmit:a,formState:{errors:r},control:n}=(0,h.mN)(),{loading:i}=(0,m.Ay)(),[l,c]=(0,o.useState)("Baptism"),[v,B]=(0,o.useState)(""),[z,L]=(0,o.useState)(""),[O,P]=(0,o.useState)(""),[M,Y]=(0,o.useState)(""),[I,R]=(0,o.useState)(""),[$,D]=(0,o.useState)(""),[V,E]=(0,o.useState)(""),[G,_]=(0,o.useState)(""),[H,K]=(0,o.useState)(""),[W,Z]=(0,o.useState)(""),[J,Q]=(0,o.useState)(""),[U,X]=(0,o.useState)(""),[ee,te]=(0,o.useState)(""),[ae,re]=(0,o.useState)(""),[ne,ie]=(0,o.useState)(""),[oe,se]=(0,o.useState)(""),[le,ce]=(0,o.useState)(""),[ue,he]=(0,o.useState)(!1),[me,de]=(0,o.useState)(!0),[xe,pe]=(0,o.useState)(!1),[fe,ge]=(0,o.useState)(!1),[ve,je]=(0,o.useState)("Save"),[be,ye]=(0,o.useState)(!1),[Ce,Ne]=(0,o.useState)(!1),ke=(0,o.useRef)(null),Ae=(0,o.useCallback)((e=>{if(" "===e.currentTarget.value)return;switch(e.currentTarget.name){case"sacrament":c(e.currentTarget.value.trim()),console.log("SACRAMENT",e.currentTarget.value.trim());break;case"neophyteFirstName":B(e.currentTarget.value.trim());break;case"neophyteLastName":L(e.currentTarget.value.trim());break;case"neophyteMiddleName":P(e.currentTarget.value.trim());break;case"email":Y(e.currentTarget.value.trim());break;case"phone":R(e.currentTarget.value.trim());break;case"cityOfBirth":D(e.currentTarget.value.trim());break;case"stateOfBirth":E(e.currentTarget.value.trim());break;case"street":_(e.currentTarget.value.trim());break;case"city":K(e.currentTarget.value.trim());break;case"state":Z(e.currentTarget.value.trim());break;case"zip":Q(e.currentTarget.value.trim());break;case"motherFirstName":X(e.currentTarget.value.trim());break;case"motherLastName":te(e.currentTarget.value.trim());break;case"motherPhoneNumber":re(e.currentTarget.value.trim());break;case"fatherFirstName":ie(e.currentTarget.value.trim());break;case"fatherLastName":se(e.currentTarget.value.trim());break;case"fatherPhoneNumber":ce(e.currentTarget.value.trim());break;default:console.log("default")}}),[c]),Se=(0,o.useCallback)((e=>{i||Ce||setTimeout((()=>{ye(!0)}),1e3)}),[i,Ce]);return(0,o.useEffect)((()=>{Se()}),[Se]),(0,q.jsx)(q.Fragment,{children:be||Ce?(0,q.jsxs)(q.Fragment,{children:[(0,q.jsx)(w,{children:be&&Ce&&(0,q.jsx)(T.A,{})}),(0,q.jsx)(b,{children:"Add Baptism Form"}),(0,q.jsxs)("form",{onSubmit:a((t=>(t=>{if(Ne(!0),console.log("data",t),""===t.sacrament)return;ye(!0);const{sacrament:a,certificate:r,baptism:n,eucharist:i,date:o,neophyteFirstName:s,neophyteLastName:l,neophyteMiddleName:c,email:h,phone:m,dob:d,cityOfBirth:x,stateOfBirth:p,street:f,city:g,state:v,zip:j,motherFirstName:b,motherLastName:y,motherPhoneNumber:C,fatherFirstName:N,fatherLastName:k,fatherPhoneNumber:A}=t;e((0,u.KI)({sacrament:a,certificate:r,baptism:n,eucharist:i,date:null===o||void 0===o?void 0:o.$d,neophyte:{firstName:s,lastName:l,middleName:c,email:h,phone:m,dob:null===d||void 0===d?void 0:d.$d,cityOfBirth:x,stateOfBirth:p,street:f,city:g,state:v,zip:j},mother:{maidenName:y,firstName:b,phone:C},father:{lastName:k,firstName:N,phone:A}})).then((e=>{console.log("Data",e.meta.arg);try{var t,a;if(clearTimeout(ke.current),ye(!1),null!==e&&void 0!==e&&null!==(t=e.error)&&void 0!==t&&t.message){const{payload:t}=e;console.log("ERROR",t),setTimeout((()=>{}),4e3)}null!==e&&void 0!==e&&null!==(a=e.error)&&void 0!==a&&a.message||(je("Saving..."),setTimeout((()=>{je("Save")}),2e3))}catch(r){console.log("error",r.message)}}))})(t))),children:[r.sacrament&&(0,q.jsx)(S,{children:"Sacrament is required."}),(0,q.jsxs)(j,{children:[(0,q.jsx)(y,{children:"General Information"}),(0,q.jsxs)(k,{children:[(0,q.jsx)(A,{...t("sacrament",{required:!0,value:l}),onChange:Ae,name:"sacrament",value:l,autoComplete:"off",type:"text",disabled:!0}),(0,q.jsx)(N,{htmlFor:1,children:"Sacrament"})]}),(0,q.jsx)(d.A,{label:"Certificate",control:(0,q.jsx)(h.xI,{name:"certificate",control:n,defaultValue:!1,value:ue,render:e=>{let{field:t}=e;return(0,q.jsx)(x.A,{...t,color:"success"})},onClick:e=>{he(e.target.checked)}})}),(0,q.jsx)(d.A,{label:"Baptism",control:(0,q.jsx)(h.xI,{name:"baptism",control:n,defaultValue:!0,value:me,render:e=>{let{field:t}=e;return(0,q.jsx)(x.A,{...t,defaultChecked:!0,color:"success"})},onClick:e=>{de(e.target.checked)}})}),(0,q.jsx)(d.A,{label:"Eucharist",control:(0,q.jsx)(h.xI,{name:"eucharist",control:n,defaultValue:!1,value:xe,render:e=>{let{field:t}=e;return(0,q.jsx)(x.A,{...t,color:"success"})},onClick:e=>{pe(e.target.checked)}})}),(0,q.jsx)(d.A,{label:"Chrismation",control:(0,q.jsx)(h.xI,{name:"chrismation",control:n,defaultValue:!1,value:fe,render:e=>{let{field:t}=e;return(0,q.jsx)(x.A,{...t,color:"success"})},onClick:e=>{ge(e.target.checked)}})}),(0,q.jsx)(h.xI,{name:"date",control:n,defaultValue:null,render:e=>{let{field:t,...a}=e;return(0,q.jsx)(g.$,{dateAdapter:f.R,children:(0,q.jsx)(p.l,{label:"Sacrament Performed",value:t.value,onChange:e=>{console.log({date:e}),t.onChange(e)},variant:"inline",autoOk:!0,format:"DD/MM/YYYY"})})}})]}),(0,q.jsxs)(j,{children:[(0,q.jsx)(y,{children:"Neophyte"}),(0,q.jsxs)(k,{children:[(0,q.jsx)(A,{...t("neophyteFirstName",{required:!0,value:v}),onChange:Ae,name:"neophyteFirstName",value:v,autoComplete:"off",type:"text"}),(0,q.jsx)(N,{htmlFor:1,children:"First Name"})]}),(0,q.jsxs)(k,{children:[(0,q.jsx)(A,{...t("neophyteLastName",{required:!0,value:z}),onChange:Ae,name:"neophyteLastName",value:z,autoComplete:"off",type:"text"}),(0,q.jsx)(N,{htmlFor:1,children:"Last Name"})]}),(0,q.jsxs)(k,{children:[(0,q.jsx)(A,{...t("neophyteMiddleName",{required:!0,value:O}),onChange:Ae,name:"neophyteMiddleName",value:O,autoComplete:"off",type:"text"}),(0,q.jsx)(N,{htmlFor:1,children:"Middle Name (optional)"})]}),(0,q.jsxs)(k,{children:[(0,q.jsx)(A,{...t("email",{required:!0,value:M}),onChange:Ae,name:"email",value:M,autoComplete:"off",type:"text"}),(0,q.jsx)(N,{htmlFor:1,children:"Email"})]}),(0,q.jsxs)(k,{children:[(0,q.jsx)(A,{...t("phone",{required:!0,value:I}),onChange:Ae,name:"phone",value:I,autoComplete:"off",type:"text"}),(0,q.jsx)(N,{htmlFor:1,children:"Phone"})]}),(0,q.jsx)(h.xI,{name:"dob",control:n,defaultValue:null,render:e=>{let{field:t,...a}=e;return(0,q.jsx)(g.$,{dateAdapter:f.R,children:(0,q.jsx)(p.l,{label:"DOB",value:t.value,onChange:e=>{console.log({date:e}),t.onChange(e)},variant:"inline",autoOk:!0,format:"DD/MM/YYYY"})})}}),(0,q.jsxs)(k,{children:[(0,q.jsx)(A,{...t("cityOfBirth",{required:!0,value:$}),onChange:Ae,name:"cityOfBirth",value:$,autoComplete:"off",type:"text"}),(0,q.jsx)(N,{htmlFor:1,children:"City of Birth"})]}),(0,q.jsxs)(k,{children:[(0,q.jsx)(A,{...t("stateOfBirth",{required:!0,value:V}),onChange:Ae,name:"stateOfBirth",value:V,autoComplete:"off",type:"text"}),(0,q.jsx)(N,{htmlFor:1,children:"State of Birth"})]}),(0,q.jsxs)(k,{children:[(0,q.jsx)(A,{...t("street",{required:!0,value:G}),onChange:Ae,name:"street",value:G,autoComplete:"off",type:"text"}),(0,q.jsx)(N,{htmlFor:1,children:"Street"})]}),(0,q.jsxs)(k,{children:[(0,q.jsx)(A,{...t("city",{required:!0,value:H}),onChange:Ae,name:"city",value:H,autoComplete:"off",type:"text"}),(0,q.jsx)(N,{htmlFor:1,children:"City"})]}),(0,q.jsxs)(k,{children:[(0,q.jsx)(A,{...t("state",{required:!0,value:W}),onChange:Ae,name:"state",value:W,autoComplete:"off",type:"text"}),(0,q.jsx)(N,{htmlFor:1,children:"State"})]}),(0,q.jsxs)(k,{children:[(0,q.jsx)(A,{...t("zip",{required:!0,value:J}),onChange:Ae,name:"zip",value:J,autoComplete:"off",type:"text"}),(0,q.jsx)(N,{htmlFor:1,children:"Zip"})]})]}),(0,q.jsxs)(j,{children:[(0,q.jsx)(y,{children:"Parents"}),(0,q.jsx)(C,{children:"Mother"}),(0,q.jsxs)(k,{children:[(0,q.jsx)(A,{...t("motherFirstName",{required:!0,value:U}),onChange:Ae,name:"motherFirstName",value:U,autoComplete:"off",type:"text"}),(0,q.jsx)(N,{htmlFor:1,children:"First Name"})]}),(0,q.jsxs)(k,{children:[(0,q.jsx)(A,{...t("motherLastName",{required:!0,value:ee}),onChange:Ae,name:"motherLastName",value:ee,autoComplete:"off",type:"text"}),(0,q.jsx)(N,{htmlFor:1,children:"Last Name"})]})," ",(0,q.jsxs)(k,{children:[(0,q.jsx)(A,{...t("motherPhoneNumber",{required:!0,value:ae}),onChange:Ae,name:"motherPhoneNumber",value:ae,autoComplete:"off",type:"text"}),(0,q.jsx)(N,{htmlFor:1,children:"Phone"})]}),(0,q.jsx)(C,{children:"Father"}),(0,q.jsxs)(k,{children:[(0,q.jsx)(A,{...t("fatherFirstName",{required:!0,value:ne}),onChange:Ae,name:"fatherFirstName",value:ne,autoComplete:"off",type:"text"}),(0,q.jsx)(N,{htmlFor:1,children:"First Name"})]}),(0,q.jsxs)(k,{children:[(0,q.jsx)(A,{...t("fatherLastName",{required:!0,value:oe}),onChange:Ae,name:"fatherLastName",value:oe,autoComplete:"off",type:"text"}),(0,q.jsx)(N,{htmlFor:1,children:"Last Name"})]})," ",(0,q.jsxs)(k,{children:[(0,q.jsx)(A,{...t("fatherPhoneNumber",{required:!0,value:le}),onChange:Ae,name:"fatherPhoneNumber",value:le,autoComplete:"off",type:"text"}),(0,q.jsx)(N,{htmlFor:1,children:"Phone"})]})]}),(0,q.jsx)(F,{type:"submit",value:be&&Ce?"Saving...":ve})]})]}):(0,q.jsx)(T.A,{})})};function z(){const{isLoggedIn:e,isVerified:t}=(0,n.A)(),{baptisms:a}=(0,i.d4)(c),h=(0,s.wA)();return(0,o.useEffect)((()=>{e&&0===a.length&&h((0,u.G7)())}),[h,e,a.length]),e&&t&&(0,q.jsxs)(l.mc,{children:[(0,q.jsx)(r.m,{children:(0,q.jsx)("title",{children:"Add Baptism"})}),(0,q.jsxs)("div",{children:[(0,q.jsx)("h1",{children:"Create Baptismal Record"}),(0,q.jsxs)("div",{children:["Total Number of Baptisms: ",a.total]})]}),(0,q.jsx)(B,{})]})}}}]);
+//# sourceMappingURL=354.99dcf49f.chunk.js.map
